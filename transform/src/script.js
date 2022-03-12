c7e07da4b7e3048d8dcc021005aec9bb16d30069
theringsofsaturn <emiliankasemi@gmail.com>
2022-03-12 18:22:18 +0100

    look at this camera and combining transformations

--- transform/src/script.js
+++ transform/src/script.js
@@ -37,7 +37,6 @@ scene.add(mesh);
 // mesh.position.set(0.7, -0.6, 1);
 // **Note: Position is a vector3
 
-
 // The position property is not any object. It's an instance of the Vector3 class. While this class has an x, a y, and a z property, it also has many useful methods.
 
 // We can get the length of a vector. The distance of the center of the scene and our object position.
@@ -87,10 +86,68 @@ scene.add(axesHelper); // it's an object and every object needs to be added to t
 mesh.rotation.x = Math.PI * 0.25;
 mesh.rotation.y = Math.PI * 0.25;
 
-// Notes** Seems easy but  when we combine those rotations, we might end up with strange results. Why? Because, while we rotate the x axis, we also change the other axes' orientation. The rotation applies in the following order: x, y, and then z. That can result in weird behaviors like one named gimbal lock when one axis has no more effect, all because of the previous ones. 
-// We can change this order by using the reorder(...) method object.rotation.reorder('yxz')
+// Notes** Seems easy but  when we combine those rotations, we might end up with strange results. Why? Because, while we rotate the x axis, we also change the other axes' orientation. The rotation applies in the following order: x, y, and then z. That can result in weird behaviors like one named gimbal lock when one axis has no more effect, all because of the previous ones.
+// We can change this order by using the reorder(...) method object.rotation.reorder('yxz') Note**: We need to add this before changing the rotation not after. So, this goes before the code above mesh.rotation.x = Math.PI * 0.25;
 
 // While Euler is easier to understand, this order problem can cause issues. And this is why most engines and 3D softwares use another solution named Quaternion.
+
+// Note**: The quaternion property also expresses a rotation, but in a more mathematical way, which solves the order problem.
+
+// COMBINING TRANSFORMATIONS
+// We can combine the position, the rotation (or quaternion), and the scale in any order. The result will be the same. It's equivalent to the state of the object.
+
+// Let's combine all the transformations we tried before:
+// mesh.position.x = 0.7
+// mesh.position.y = - 0.6
+// mesh.position.z = 1
+// mesh.scale.x = 2
+// mesh.scale.y = 0.25
+// mesh.scale.z = 0.5
+// mesh.rotation.x = Math.PI * 0.25
+// mesh.rotation.y = Math.PI * 0.25
+
+// SCENE GRAPH
+// At some point, we might want to group things. Let's say we are building a house with walls, doors, windows, a roof, bushes, etc.
+
+// When we think we're done, we become aware that the house is too small, and we have to re-scale each object and update their positions.
+
+// A good alternative would be to group all those objects into a container and scale that container.
+
+// we can do that with the Group class.
+
+// Instantiate a Group and add it to the scene. Now, when we want to create a new object, we can add it to the Group we just created using the add(...) method rather than adding it directly to the scene
+
+// Because the Group class inherits from the Object3D class, it has access to the previously-mentioned properties and methods like position, scale, rotation, quaternion, and lookAt.
+
+// Comment the lookAt(...) call and, instead of our previously created cube, create 3 cubes and add them to a Group. Then apply transformations on the group:
+// Note**: The order doesn't really matter, as long as it's valid JavaScript.
+
+// GROUP OF OBJECTS
+//  const group = new THREE.Group()
+//  group.scale.y = 2
+//  group.rotation.y = 0.2
+//  scene.add(group)
+
+//  const cube1 = new THREE.Mesh(
+//      new THREE.BoxGeometry(1, 1, 1),
+//      new THREE.MeshBasicMaterial({ color: 0xff0000 })
+//  )
+//  cube1.position.x = - 1.5
+//  group.add(cube1)
+
+//  const cube2 = new THREE.Mesh(
+//      new THREE.BoxGeometry(1, 1, 1),
+//      new THREE.MeshBasicMaterial({ color: 0xff0000 })
+//  )
+//  cube2.position.x = 0
+//  group.add(cube2)
+
+//  const cube3 = new THREE.Mesh(
+//      new THREE.BoxGeometry(1, 1, 1),
+//      new THREE.MeshBasicMaterial({ color: 0xff0000 })
+//  )
+//  cube3.position.x = 1.5
+//  group.add(cube3)
 
 // SIZES
 const sizes = {
@@ -104,6 +161,19 @@ camera.position.z = 3;
 scene.add(camera);
 
 console.log(mesh.position.distanceTo(camera.position));
+
+// LOOK AT THIS
+// Object3D instances have an excellent method named lookAt(...) that let us ask an object to look at something. The object will automatically rotate its -z axis toward the target we provided. No complicated maths needed.
+
+// We can use it to rotate the camera toward an object, orientate a cannon to face an enemy, or move the character's eyes to an object.
+
+// The parameter is the target and must be a Vector3.
+// camera.lookAt(new THREE.Vector3(0, - 1, 0))
+
+// The cube seems to be higher, but in fact, the camera is looking below the cube.
+
+// We can also use any existing Vector3 such as the mesh's position, but that will result in the default camera position because our mesh is in the center of the scene.
+camera.lookAt(mesh.position);
 
 // RENDERER
 const renderer = new THREE.WebGLRenderer({
