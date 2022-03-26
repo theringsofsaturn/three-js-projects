ae331f94c4208ea3a4d06e23af38af15990841e8
theringsofsaturn <emiliankasemi@gmail.com>
2022-03-26 02:26:19 +0100

    displayed text in the scene

--- text/src/script.js
+++ text/src/script.js
@@ -4,6 +4,7 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import * as dat from "lil-gui";
 import typefaceFont from "three/examples/fonts/helvetiker_regular.typeface.json";
 import { FontLoader } from "three/examples/jsm/loaders/FontLoader.js";
+import { TextGeometry } from "three/examples/jsm/geometries/TextGeometry.js";
 
 /**
  * Base
@@ -28,18 +29,25 @@ const textureLoader = new THREE.TextureLoader();
 const fontLoader = new FontLoader();
 
 fontLoader.load("/fonts/helvetiker_regular.typeface.json", (font) => {
-  console.log("loaded");
+  const textGeometry = new TextGeometry("Emilian Kasemi", {
+    font: font,
+    size: 0.5,
+    height: 0.2,
+    curveSegments: 12,
+    bevelEnabled: true,
+    bevelThickness: 0.03,
+    bevelSize: 0.02,
+    bevelOffset: 0,
+    bevelSegments: 5,
+  });
+  const textMaterial = new THREE.MeshBasicMaterial({ wireframe: true });
+  const text = new THREE.Mesh(textGeometry, textMaterial);
+  scene.add(text);
 });
 
 /**
  * Object
  */
-const cube = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1),
-  new THREE.MeshBasicMaterial()
-);
-
-scene.add(cube);
 
 /**
  * Sizes
